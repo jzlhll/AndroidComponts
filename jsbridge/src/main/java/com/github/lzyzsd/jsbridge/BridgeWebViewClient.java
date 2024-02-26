da88b3cbdfee8f4dbac1c9211e8b4d12ae5b5de7
allan.jiang <allan.jiang@typhur.com>
2024-02-26 10:31:05 +0800

    jsbridge 2. 以前代码通过onPageFinish才进行加载插入js代码段，某些网址可能无法及时调用，改成onPageStart等待几百毫秒自动插入。

--- jsbridge/src/main/java/com/github/lzyzsd/jsbridge/BridgeWebViewClient.java
+++ jsbridge/src/main/java/com/github/lzyzsd/jsbridge/BridgeWebViewClient.java
@@ -1,6 +1,8 @@
 package com.github.lzyzsd.jsbridge;
 
 import android.graphics.Bitmap;
+import android.util.Log;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -11,14 +13,37 @@ import java.net.URLDecoder;
  * Created by bruce on 10/28/15.
  */
 public class BridgeWebViewClient extends WebViewClient {
-    private BridgeWebView webView;
+    private final BridgeWebView webView;
+
+    private boolean isLoadedBridgeJs = false;
 
     public BridgeWebViewClient(BridgeWebView webView) {
         this.webView = webView;
     }
 
+//    @Override
+//    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//        try {
+//            url = URLDecoder.decode(url, "UTF-8");
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        }
+//
+//        if (url.startsWith(BridgeUtil.YY_RETURN_DATA)) { // 如果是返回数据
+//            webView.handlerReturnData(url);
+//            return true;
+//        } else if (url.startsWith(BridgeUtil.YY_OVERRIDE_SCHEMA)) { //
+//            webView.flushMessageQueue();
+//            return true;
+//        } else {
+//            return super.shouldOverrideUrlLoading(view, url);
+//        }
+//    }
+
     @Override
-    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        var url = request.getUrl().toString();
+        //Log.d(BridgeUtil.TAG, "shouldOverride UrlLoading: " + url);
         try {
             url = URLDecoder.decode(url, "UTF-8");
         } catch (UnsupportedEncodingException e) {
@@ -32,34 +57,42 @@ public class BridgeWebViewClient extends WebViewClient {
             webView.flushMessageQueue();
             return true;
         } else {
-            return super.shouldOverrideUrlLoading(view, url);
+            return super.shouldOverrideUrlLoading(view, request);
         }
     }
 
     @Override
     public void onPageStarted(WebView view, String url, Bitmap favicon) {
         super.onPageStarted(view, url, favicon);
+        if (!isLoadedBridgeJs) {
+            webView.postDelayed(() -> {
+                if (!isLoadedBridgeJs && webView.isAttachedToWindow()) {
+                    isLoadedBridgeJs = true;
+                    loadJs(webView);
+                }
+            }, 500);
+        }
     }
 
     @Override
     public void onPageFinished(WebView view, String url) {
         super.onPageFinished(view, url);
-
-        if (BridgeWebView.toLoadJs != null) {
-            BridgeUtil.webViewLoadLocalJs(view, BridgeWebView.toLoadJs);
+        if (!isLoadedBridgeJs) {
+            isLoadedBridgeJs = true;
+            loadJs(view);
         }
+    }
 
-        //
+    public void loadJs(WebView view) {
+        Log.d(BridgeUtil.TAG, "load js!!");
+        BridgeUtil.webViewLoadLocalJs(view, BridgeWebView.toLoadJs);
+        Log.d(BridgeUtil.TAG, "load js end!!");
+
         if (webView.getStartupMessage() != null) {
             for (Message m : webView.getStartupMessage()) {
                 webView.dispatchMessage(m);
             }
             webView.setStartupMessage(null);
         }
-    }
-
-    @Override
-    public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-        super.onReceivedError(view, errorCode, description, failingUrl);
     }
 }
