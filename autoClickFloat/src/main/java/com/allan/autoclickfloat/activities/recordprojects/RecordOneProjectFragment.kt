0c50e968fcc8e26ae03b93c0c89c358c4ee7aae0
allan.jiang <allan.jiang@typhur.com>
2024-04-28 18:09:40 +0800

    update

--- autoClickFloat/src/main/java/com/allan/autoclickfloat/activities/recordprojects/RecordOneProjectFragment.kt
+++ autoClickFloat/src/main/java/com/allan/autoclickfloat/activities/recordprojects/RecordOneProjectFragment.kt
@@ -1,6 +1,7 @@
 package com.allan.autoclickfloat.activities.recordprojects
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -9,10 +10,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.allan.autoclickfloat.consts.Const
 import com.allan.autoclickfloat.database.Step
 import com.allan.autoclickfloat.databinding.RecordProjectOneFragmentBinding
+import com.au.module_android.click.onClick
 import com.au.module_android.ui.FragmentRootActivity
 import com.au.module_android.ui.bindings.BindingParamsFragment
+import com.au.module_android.utils.gone
 import com.au.module_android.utils.launchOnThread
 import com.au.module_android.utils.unsafeLazy
+import com.au.module_android.utils.visible
 
 class RecordOneProjectFragment : BindingParamsFragment<RecordProjectOneFragmentBinding>() {
     companion object {
@@ -42,27 +46,46 @@ class RecordOneProjectFragment : BindingParamsFragment<RecordProjectOneFragmentB
         val isAddMode = getTempParams<Boolean>("isAddMode") ?: false
         if (isAddMode) {
             binding.toolBar.title.text = "新建方案"
+            binding.descCenter.visible()
+            binding.descBottom.gone()
+            binding.addStepCenterBtn.visible()
+            binding.deleteStepBtn.gone()
         } else {
             binding.toolBar.title.text = projectName ?: ""
+            binding.descCenter.gone()
+            binding.descBottom.visible()
+            binding.addStepCenterBtn.gone()
+            binding.deleteStepBtn.visible()
+        }
+
+        binding.deleteStepBtn.onClick {
+            viewModel.switchDeleteMode()
         }
 
         viewModel.stepsData.observe(this) {
-
+            refreshRcv(it)
         }
 
+        binding.addStepCenterBtn.onClick {
+            addStep()
+        }
         if (projectId != null && projectId >= 0) {
             viewModel.loadProjectIdSteps(projectId)
         }
+    }
 
+    private fun addStep() {
+        val appList = requireActivity().packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
 
     }
 
-    private fun refreshRcv(steps:List<Step>) {
+    private fun refreshRcv(steps:List<StepWrap>) {
         if (adapter == null) {
             binding.rcv.adapter = RecordOneProjectAdapter().also { adapter = it }
             binding.rcv.layoutManager = LinearLayoutManager(requireContext()).also { it.orientation = LinearLayoutManager.VERTICAL }
             binding.rcv.setHasFixedSize(true)
         }
 
+        adapter?.initDatas(steps, false)
     }
 }
--- autoClickFloat/src/main/java/com/allan/autoclickfloat/activities/recordprojects/RecordOneProjectViewModel.kt
+++ autoClickFloat/src/main/java/com/allan/autoclickfloat/activities/recordprojects/RecordOneProjectViewModel.kt
@@ -4,18 +4,27 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.allan.autoclickfloat.database.AppDatabase
-import com.allan.autoclickfloat.database.Step
 import com.au.module_android.simplelivedata.SafeLiveData
 import com.au.module_android.simplelivedata.asSafeLiveData
 import com.au.module_android.utils.launchOnThread
 
 class RecordOneProjectViewModel : ViewModel() {
-    val stepsData:LiveData<List<Step>> = SafeLiveData()
+    val stepsData:LiveData<List<StepWrap>> = SafeLiveData()
+
+    private var isDeleteMode = false
 
     fun loadProjectIdSteps(projectId:Int) {
         viewModelScope.launchOnThread {
-            val steps = AppDatabase.db.stepDao().getAll(projectId)
+            val steps = AppDatabase.db.stepDao().getAll(projectId).map {
+                StepWrap(false, it)
+            }
             stepsData.asSafeLiveData().setValueSafe(steps)
         }
+    }
+
+    fun switchDeleteMode() {
+        isDeleteMode = !isDeleteMode
+        val list = stepsData.value ?: listOf()
+        stepsData.asSafeLiveData().setValueSafe(list.map { StepWrap(isDeleteMode, it.step) })
     }
 }
